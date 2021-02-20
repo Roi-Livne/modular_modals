69254586b2a6cef56b094a98a61cc2446d23a4aa
Roi-Livne <roilivne14@gmail.com>
2021-02-20 12:18:49 +0200

    modified App.js for opening Modal types

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import React, { useState } from 'react';
+import Modal from './components/Modal'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const ModalContext = React.createContext(null)
+
+
+export function App() {
+  const [modal, setModal] = useState({ isOpen: false, type: 'login' })
+
+  const openModal = (type) => {
+    setModal({ isOpen: true, type })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Modular React Modals</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Choose a modal to open
+    <ModalContext.Provider value={{ modal, openModal }}>
+      <Modal />
+      <div className="App">
+        <header className="App-header">
+          <h1>Modular React Modals</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Choose a modal to open
         </p>
-        <button>Sign in</button>
-        <button>Sign up</button>
-        <button>Buy now</button>
-        <button>Credits</button>
-      </header>
-    </div>
+          <button onClick={() => openModal('login')}>Log in</button>
+          <button onClick={() => openModal('signup')}>Sign up</button>
+          <button onClick={() => openModal('buy')}>Buy now</button>
+          <button onClick={() => openModal('credits')}>Credits</button>
+        </header>
+      </div>
+    </ModalContext.Provider>
   );
 }
-
-export default App;
